9400a94468ffbbd8a3599afe4de5d88d7fb8f55d
Avesh Raj Singh <aveshrajsingh3@gmail.com>
2025-12-05 20:26:55 +0530

    feat: enhance signup and OTP verification functionality

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,13 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Add request logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`, req.body);
+  next();
+});
+
 import userRouter from './routes/user.route.js';
 app.use("/users", userRouter);
 
